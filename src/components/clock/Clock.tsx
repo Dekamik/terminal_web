891e3e7e35d0107c8a7250577d7f9c3878a64b98
Dennis von Bargen <dennis@svrvive.com>
2021-01-03 16:14:16 +0100

    Clock implemented

--- src/components/clock/Clock.tsx
+++ src/components/clock/Clock.tsx
@@ -1,15 +1,29 @@
+import moment from 'moment';
+import 'moment/locale/sv'
 import * as React from 'react';
 
 export const Clock: React.FunctionComponent = () => {
 
+    const [date, setDate] = React.useState<string>("...");
+    const [time, setTime] = React.useState<string>("...");
+
+    React.useEffect(() => {
+        moment.locale('sv');
+        setInterval(() => {
+            let timestamp = moment();
+            setDate(timestamp.format("Do MMMM YYYY"));
+            setTime(timestamp.format("HH:mm"));
+        }, 1000);
+    }, []);
+
     return (
         <>
-            <div className="col-12 clock">
+            <div className="clock">
                 <div className="row">
-                    <div className="col-12 clock-date">3:e januari 2021</div>
+                    <div className="col-12 clock-date">{date}</div>
                 </div>
                 <div className="row">
-                    <div className="col-12 clock-time">12:34</div>
+                    <div className="col-12 clock-time">{time}</div>
                 </div>
             </div>
         </>
