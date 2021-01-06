7bcaa830c99849a3812047b38731c3514bcdf8f9
Dennis von Bargen <dennis@svrvive.com>
2021-01-07 00:34:18 +0100

    Turned items in deviations modal into accordions

--- src/components/public-transport/SLNextDepartures.tsx
+++ src/components/public-transport/SLNextDepartures.tsx
@@ -20,6 +20,7 @@ interface IDepartureItem {
 }
 
 interface IDeviationItem {
+    id: string;
     header: string;
     details: string;
     lines: string;
@@ -101,6 +102,7 @@ export const SLNextDepartures: React.FunctionComponent = () => {
             api.getDeviations(deviationsModes, deviationsLines, 
                 (data: IDeviationsResponse) => {
                     setDeviations(data.ResponseData.map((item: IDeviations) => ({
+                        id: item.DevCaseGid.toString(),
                         header: item.Header,
                         details: item.Details,
                         lines: item.Scope,
@@ -231,25 +233,33 @@ export const SLNextDepartures: React.FunctionComponent = () => {
                 {
                     deviations
                         ? deviations.map((item: IDeviationItem) => 
-                            <div className="col-12">
-                                <div className={`row ${getSeverityClasses(item)}`}>
-                                    <h5 className="text-center mx-auto">{item.header}</h5>
-                                </div>
-                                <div className="row">
-                                    <p>
-                                        {item.details}
-                                    </p>
-                                </div>
-                                <div className="row">
-                                    <p>
-                                        Detta påverkar {item.lines} mellan 
-                                        {
-                                            isSameDate(item.dateFrom, item.dateTo) ? ` ${moment(item.dateFrom).format("HH:mm")} och ${moment(item.dateTo).format("HH:mm")} den ${moment(item.dateTo).format("D/M")}`
-                                                : isSameYear(item.dateFrom, item.dateTo) ? ` ${moment(item.dateFrom).format("HH:mm D/M")} och ${moment(item.dateTo).format("HH:mm D/M")}`
-                                                : ` ${moment(item.dateFrom).format("HH:mm D/M YYYY")} och ${moment(item.dateTo).format("HH:mm D/M YYYY")}`
-                                                
-                                        }
-                                    </p>
+                            <div className="col-12" id="accordion">
+                                <div className="card">
+                                    <div className={`card-header ${getSeverityClasses(item)}`} id={`header${item.id}`} data-toggle="collapse" data-target={`#details${item.id}`} aria-expanded="false" aria-controls={`details${item.id}`}>
+                                        <h5 className="text-center mx-auto">
+                                            {item.header}
+                                        </h5>
+                                    </div>
+                                    <div id={`details${item.id}`} className="collapse" aria-labelledby={`header${item.id}`} data-parent="#accordion">
+                                        <div className="card-body">
+                                            <div className="row">
+                                                <p>
+                                                    {item.details}
+                                                </p>
+                                            </div>
+                                            <div className="row">
+                                                <p>
+                                                    Detta påverkar {item.lines} mellan 
+                                                    {
+                                                        isSameDate(item.dateFrom, item.dateTo) ? ` ${moment(item.dateFrom).format("HH:mm")} och ${moment(item.dateTo).format("HH:mm")} den ${moment(item.dateTo).format("D/M")}`
+                                                            : isSameYear(item.dateFrom, item.dateTo) ? ` ${moment(item.dateFrom).format("HH:mm D/M")} och ${moment(item.dateTo).format("HH:mm D/M")}`
+                                                            : ` ${moment(item.dateFrom).format("HH:mm D/M YYYY")} och ${moment(item.dateTo).format("HH:mm D/M YYYY")}`
+                                                            
+                                                    }
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         )
